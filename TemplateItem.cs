35eebd33d2fa4f655e81934cbbb4fb7131b854a5
Marcos Martínez Robles <marcosmartinezrobles@outlook.com>
2020-10-17 11:46:27 +0200

    Add project files.

---
+++ TemplateItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorJSONTransform
+{
+    public class TemplateItem
+    {
+        public string FieldName { get; set; }
+        public string JsonPath { get; set; }
+        public string FixedValue { get; set; }
+        public bool IsArray { get; set; }
+        public bool IsFixedValue { get; set; }
+        public List<TemplateItem> Child { get; set; }
+    }
+}
